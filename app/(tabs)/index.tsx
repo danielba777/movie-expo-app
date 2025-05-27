db5c1a750774975a1fbd100d912a820b017260a6
Daniel <danielbauer230501@gmail.com>
2025-05-27 17:58:25 +0200

    Integrated Fetch Movies Logic

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,10 +1,33 @@
+import SearchBar from "@/components/SearchBar";
+import { icons } from "@/constants/icons";
+import { images } from "@/constants/images";
+import { fetchPopularMovies } from "@/services/api";
+import { useFetch } from "@/services/useFetch";
+import { useRouter } from "expo-router";
 import React from "react";
-import { Text, View } from "react-native";
+import { Image, ScrollView, View } from "react-native";
 
 const index = () => {
+  const router = useRouter();
+
+  const { data: movies } = useFetch(() => fetchPopularMovies({ query: "" }));
+
   return (
-    <View>
-      <Text></Text>
+    <View className="flex-1 bg-primary">
+      <Image source={images.bg} className="absolute w-full z-0" />
+      <ScrollView
+        className="flex-1 px-5"
+        showsVerticalScrollIndicator={false}
+        contentContainerStyle={{ minHeight: "100%", paddingBottom: 10 }}
+      >
+        <Image source={icons.logo} className="w-12 h-10 mt-20 mb-5 mx-auto" />
+        <View className="flex-1 mt-5">
+          <SearchBar
+            onPress={() => router.push("/search")}
+            placeholder="Search for a movie"
+          />
+        </View>
+      </ScrollView>
     </View>
   );
 };
